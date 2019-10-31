bec6a4ea51441d7fc29b5788a5cfcc0887913d9a
jtamakela <janne.makela@uef.fi>
2019-10-31 15:41:32 +0200

    Added saving option and cleaned up a bit

--- CECTVOI_Rotate.m
+++ CECTVOI_Rotate.m
@@ -1,4 +1,4 @@
-function angles = CECTVOI_Rotate(whichfile, whichlocation)
+function  NEW_CECT = CECTVOI_Rotate
 
 %% m-file for further rotating the earlier created CECT image VOIs
 % Uses CECTVOI_Alignment and find_demarcation to turn
@@ -8,20 +8,68 @@ function angles = CECTVOI_Rotate(whichfile, whichlocation)
 %% (c) Janne T.A. Mäkelä, October / 2019
 
 
-clear all, close all, clc; 
+% clear all, close all, clc;
+
+% Are we saving?
+saving = 0; % 1 for saving
+
 
 %Testing if this returns the angles
 
-whichfile = 1;  %Which sample
-whichlocation = 1; %Which of the measured locations
-
-test = CECTVOI_Alignment(whichfile, whichlocation)
+filelist = dir('*VOI_data*.mat');
 
 
 
-
-
-
+for whichfile = 7%:length(filename);  %Which sample
+    
+    
+    % Going through all the timepoints
+    filename = filelist(whichfile).name %Reads the last mat file
+    
+    load(filename); %Imports DATA
+    
+    % DATA{1,2} has the VOIs stored
+    CECT_original = DATA{1,2}; %2 energies, 6 locations, 8 timepoints
+    
+    
+    for whichlocation = 1:size(CECT_original,1) % %Which of the measured locations
+        
+        %Check the angle
+        angles = CECTVOI_Alignment(whichfile, whichlocation);
+        
+        
+        % Going through all timepoints & locations
+        h2 = waitbar(0,'Rotating the files, please wait...'); %Display waitbar
+        
+        for whichtimepoint = 1:16
+            waitbar(whichtimepoint/length(DATA{1,2}));
+            
+            % Rotate X
+            new_CECT90 = imrotate3(CECT_original{whichlocation, whichtimepoint}, angles(1), [1 0 0]);
+            % Turning 90 deg
+            new_rot_CECT90 = rot90(new_CECT90);
+            % Rotate Y
+            new_new_rot_CECT90 = imrotate3(new_rot_CECT90, angles(2), [1 0 0]);
+            % Turning back to original
+            NEW_CECT{whichlocation, whichtimepoint} = rot90(new_new_rot_CECT90,-1);
+            
+        end
+        close(h2)
+        
+    end
+    
+    savename = filename(1:end-14);
+    
+    NEWDATA = {DATA{1,1}, NEW_CECT, DATA{1,3}};
+    
+    % Don't overwrite
+    if saving == 1
+        
+        save([savename, '__RotatedVOI_data', num2str(length(dir('*_RotatedVOI_data*.mat'))+1), '.mat'], 'NEWDATA');
+    end
+    
+    
+    
 end
 
 
